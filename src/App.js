addf46fdcfbfc67405d1e4d29cb37b1aa436cff6
Manpreet <manpreet62303@gmail.com>
2024-11-26 12:13:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './Form';
+import Home from './Home';
+
+function App() {
+ 
+     
+
+  return (
+    <div className="App">
+      
+      <Home /> 
+      <Form />
+    </div>
+  );
+
+}
+
+export default App;
