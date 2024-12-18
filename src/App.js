111e287bc5ff7e89aeb49f393871bab95fbd8f83
Manpreet <manpreet62303@gmail.com>
2024-12-18 12:22:20 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import Form from './Form';
-import Home from './Home';
-import Input from './Input';
-function App() {
+import React from "react"
+import Navbar from './Components/Navbar'
+import Product from './Components/Product'
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+import ProductDetail from "./Components/ProductDetail"
+import SearchItem from "./Components/SearchItem"
+import Cart from "./Components/Cart"
+import { items } from "./Components/Data"
  
-     
 
-  return (
-    <div className="App">
- <Form />
- <Input />
- {/* <Home /> */}
-    </div>
-  );
-
+const App = () =>{
+    const [data, setData] = useState(...items)
+    return(
+        <>
+        <Router>
+<Navbar />
+<Routes>
+    <Route path="/" element={<Product items={data} /> }/>
+    <Route path="/product/:id" element={<ProductDetail />}/>
+    <Route path="/search/:term" element={<SearchItem />}/>
+    <Route path="/cart" element={<Cart />}/>
+</Routes>
+<Product />
+</Router>
+        </>
+    )
 }
-
-export default App;
+export default App
