d23eea6dca5e58619195f1207d17bfb9587137fa
Manpreet <manpreet62303@gmail.com>
2024-12-20 13:10:09 +0530

    e-cart

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React from "react"
-import Navbar from './Components/Navbar'
-import Product from './Components/Product'
-import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
-import ProductDetail from "./Components/ProductDetail"
-import SearchItem from "./Components/SearchItem"
-import Cart from "./Components/Cart"
-import { items } from "./Components/Data"
- 
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar"; 
+import Product from "./component/Product";
+import ProductDetail from "./component/ProductDetail";
+import SearchItem from "./component/SearchItem";
+import Cart from "./component/Cart";
+import { items } from "./component/Data"; 
 
-const App = () =>{
-    const [data, setData] = useState(...items)
-    return(
-        <>
-        <Router>
-<Navbar />
-<Routes>
-    <Route path="/" element={<Product items={data} /> }/>
-    <Route path="/product/:id" element={<ProductDetail />}/>
-    <Route path="/search/:term" element={<SearchItem />}/>
-    <Route path="/cart" element={<Cart />}/>
-</Routes>
-<Product />
-</Router>
-        </>
-    )
-}
-export default App
+
+const App = () => {
+  const [data, setData] = useState(items);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  return (
+    <>
+    <Router>
+    <Navbar cart={cart} setData={setData} />
+    <Routes>
+      <Route path="/" element={<Product cart={cart} setCart={setCart} items={data} />} />
+      <Route path="/product/:id" element={<ProductDetail cart={cart} setCart={setCart} />} />
+      <Route path="/search/:term" element={<SearchItem cart={cart} setCart={setCart} />} />
+      <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
+    </Routes>
+  
+    </Router>
+    </>
+  );
+};
+
+export default App;
